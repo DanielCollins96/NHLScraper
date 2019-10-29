e823644ec08060dac999a5bfa9fa263e57ee6aca
Daniel Collins <danielmcollins1996@gmail.com>
2019-10-28 21:36:34 -0700

    made splitter th '#' will be ignored, and an empty message aborts the commit.

---
+++ SplitNames.py
@@ -0,0 +1,43 @@
+import json
+
+# split_names{'firstname', 'lastname'}
+split_names = {}
+# input("Enter the filename: ")
+path = 'FlamesRecords.json'
+outputfile = input("Enter filename ") + ".json"
+
+input_file = open(path, 'r')
+
+# with open(path) as f:
+#     for line in f:
+#         print(json.loads(line))
+#     f.close()
+
+
+# with open(path, 'r') as f:
+data = json.load(input_file)
+
+print(len(data['roster']))
+
+rosterSize = len(data['roster'])
+
+x = 0
+while x < rosterSize:
+    fname = data['roster'][x]["name"].split(" ")[0]
+    lname = data['roster'][x]["name"].split(" ")[1]
+    data['roster'][x]["firstname"] = fname
+    data['roster'][x]["lastname"] = lname
+    print(fname)
+    print(lname)
+    # print(data['roster'][x]["name"])
+    x += 1
+
+print(data['roster'][0])
+# for x in data:
+#     print(data["roster"]["name"])
+# print(input_file.read())
+
+
+with open(outputfile, 'w') as outfile:
+    json.dump(data, outfile)
+outfile.close()
