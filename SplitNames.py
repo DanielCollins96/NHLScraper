a348cd7891b801f6d16c82c82e398f55d4daddd3
Daniel Collins <danielmcollins1996@gmail.com>
2019-10-29 19:44:05 -0700

    cleanin da work fr

--- SplitNames.py
+++ SplitNames.py
@@ -4,7 +4,7 @@ import json
 split_names = {}
 # input("Enter the filename: ")
 path = 'FlamesRecords.json'
-outputfile = input("Enter filename ") + ".json"
+outputfile = "./datamang/" + input("Enter filename ") + ".json"
 
 input_file = open(path, 'r')
 
@@ -27,12 +27,15 @@ while x < rosterSize:
     lname = data['roster'][x]["name"].split(" ")[1]
     data['roster'][x]["firstname"] = fname
     data['roster'][x]["lastname"] = lname
-    print(fname)
-    print(lname)
+    print(data['roster'][x])
+
+    del data['roster'][x]["team"]
+    # print(fname)
+    # print(lname)
     # print(data['roster'][x]["name"])
     x += 1
 
-print(data['roster'][0])
+# print(data['roster'][0])
 # for x in data:
 #     print(data["roster"]["name"])
 # print(input_file.read())
